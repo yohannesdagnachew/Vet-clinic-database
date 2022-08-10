00b4d5102871c517e5065ff2520cd1d3ff22e002
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-08-10 22:09:14 +0300

    Solve multi table challenge

--- data.sql
+++ data.sql
@@ -12,3 +12,23 @@ INSERT INTO animals  VALUES ('Boarmon','Jun 7 2005', 20.4, true, 7);
 INSERT INTO animals  VALUES ('Blossom','Oct 13 1998', 17.0, true, 3);
 INSERT INTO animals  VALUES ('Ditto','May 14 2022', 22.0, true, 4);
 
+INSERT INTO owners(full_name, age) VALUES ('Sam Smith', 34);
+INSERT INTO owners(full_name, age) VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners(full_name, age) VALUES ('Bob', 45);
+INSERT INTO owners(full_name, age) VALUES ('Melody Pond', 77);
+INSERT INTO owners(full_name, age) VALUES ('Dean Winchester', 14);
+INSERT INTO owners(full_name, age) VALUES ('Jodie Whittaker', 38);
+
+INSERT INTO species(name) VALUES ('Pokemon');
+INSERT INTO species(name) VALUES ('Digimon');
+
+Update animals  SET species_id = 'digimon' WHERE name LIKE '%mon';
+Update animals  SET species_id='pokemon' WHERE species IS NUll;
+
+UPDATE animals set owner_id = 1 where name = 'Agumon';
+UPDATE animals set owner_id = 2 where name in ('Gabumon', 'Pikachu');
+UPDATE animals set owner_id = 3 where name in ('Devimon', 'Plantmon');
+UPDATE animals set owner_id = 4 where name in ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals set owner_id = 5 where name in ('Angemon', 'Boarmon');
+
+
--- queries.sql
+++ queries.sql
@@ -9,24 +9,6 @@ SELECT * FROM animals WHERE neutered=true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
-Update animals set species='unspecified';
-ROLLBACK;
-Update animals  SET species='digimon' WHERE name LIKE '%mon';
-Update animals  SET species='pokemon' WHERE species IS NUll;
-
-BEGIN;
-DELETE FROM animals; 
-ROLLBACK;
-Select *from animals;
-
-
-Delete from animals where date_of_birth > 'Jan 1 2022';
-SAVEPOINT save1;
-UPDATE animals set weight_kg= weight_kg * -1 where weight_kg < 0;
-ROLLBACK;
-UPDATE animals set weight_kg= weight_kg * -1 where weight_kg < 0;
-Commit;
-
 
 SELECT COUNT(*) FROM animals;
 SELECT COUNT(escape_attempts) FROM animals where escape_attempts = 0;
@@ -36,3 +18,16 @@ SELECT species,  Min(weight_kg) FROM animals Group by species;
 SELECT  AVG(escape_attempts) FROM animals where date_of_birth BETWEEN 'Jan 01 1990' AND 'Jan 01 2000';
 
 
+SELECT name FROM animals a JOIN owners o ON o.Id = a.id;
+SELECT name,full_name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Melody Pond';
+SELECT animals.name, species.name FROM animals JOINnimals.species_id = species.id WHERE species.name = 'Pokemon';
+Select owners.full_name, animals.name FROM owners JOIN animals on animals.owner_id = owners.id;
+SELECT species.name, COUNT(*) FROM animals JOIN species on animals.species_id=species.id GROUP BY species.name;
+SELECT animals.name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Jennifer Orwell';
+Select animals.name, owners.full_name, species.name FROM animals JOIN owners on animals.owner_id = owners.id 
+                                 JOIN species on animals.species_id = species.id
+                                 where species.name = 'Digimon' AND owners.full_name='Jennifer Orwell';
+SELECT animals.name,animals.escape_attempts,owners.full_name FROM animals JOIN owners ON animals.owner_id = owners.id
+  WHERE animals.escape_attempts = 0 AND owners.full_name = 'Dean Winchester';                                
+SELECT owners.full_name as owner_full_name,COUNT(animals.name) AS Number_of_animals
+FROM owners LEFT JOIN animals ON owners.id = animals.owner_id GROUP BY owner_full_name ORDER BY Number_of_animals DESC;
